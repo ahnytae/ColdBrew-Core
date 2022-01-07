f6efe84900a20e160ec9fa2748988a7fb61e781a
ahnytae <h.t.ahn93@gmail.com>
2022-01-07 23:25:06 +0900

    fix: stun URL 변경

--- lib/signaling/SignalingController.ts
+++ lib/signaling/SignalingController.ts
@@ -9,13 +9,7 @@ export class SignalingController extends ColdBrew {
   private static readonly STUN_URL = {
     iceServers: [
       {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
-        ],
+        urls: "stun:relay.webwormhole.io:3478",
       },
     ],
   };
